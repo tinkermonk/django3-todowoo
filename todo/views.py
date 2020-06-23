35d52d871b2567d7834b37c8a9d170e9a38ce48d
Yassif <yassif.n.m@gmail.com>
2020-06-23 15:50:49 +0800

    created a function in views to handle the request to create a user and the database will take it. It works however there are errors as to what happens when there is a successful signup as well as well as the error when there is already a user that has taken a name, so duplicated names.

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 # Create your views here.
 def signupuser(request):
-    return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            user.save()
+        else:
+            print("The passwords did not match")
