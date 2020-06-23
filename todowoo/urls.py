6d31aaa6de0ad84293e724e1bc50736a67cb8909
Yassif <yassif.n.m@gmail.com>
2020-06-23 17:31:46 +0800

    created a function to handle the error handling as well as a redirect to the app page when a user has logged in. Therefore created base.html as well as the app html page. the functions are called in the views.py file.

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -21,8 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     #Auth
-    path('signup/', views.signupuser, name='signupuser')
+    path('signup/', views.signupuser, name='signupuser'),
 
     #todos
-
+    path('current/', views.currenttodos, name='currenttodos'),
 ]
