0a642ea9107a55296e9f75b9388e600808c4d709
Yassif <yassif.n.m@gmail.com>
2020-06-26 12:01:27 +0800

    Created a custom form to take in the todo items. So created a forms.py file in the app. Then imported it into views.py and created a function to handle the request to going to the createtodo page. Since Django does not have forms that are custom, that is why you create a forms.py file to handle the input to display on that page. the function takes in the get request and displays the page. else it will post the information that is input into the form that is displayed. It will save the elements that need explicit input by the user and save it and then redirect it to the currenttodos page. A value error handling was added in the event that any bad input was made that exceeded the parameters of the form. A createtodo page was also created.

--- todo/views.py
+++ todo/views.py
@@ -3,10 +3,13 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
 # Create your views here.
+
 
 def home(request):
     return render(request, 'todo/home.html')
+
 
 def signupuser(request):
     if request.method == 'GET':
@@ -25,6 +28,8 @@ def signupuser(request):
         else:
             return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':'Passwords did '
                                                                                                 'not match'})
+
+
 def loginuser(request):
     if request.method == 'GET':
         return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
@@ -36,10 +41,26 @@ def loginuser(request):
             login(request, user)
             return redirect('currenttodos')
 
+
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
+
+
+def createtodo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form': TodoForm()})
+    else:
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error':'Bad Data Entry. Try Again.'})
+
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
